71622d21d54df445a71bd8a3154b7b93ecedf734
Mustafa Muse <umpp101@icloud.com>
2019-12-02 13:58:21 -0800

    Adding Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
     has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"    
     # source: :followee matches with the belong_to :followee identification in the Follow model   
     has_many :followees, through: :followee_follows, source: :followee
+
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :password_confirmation, confirmation: true
+    validates :age, numericality: true
+
 end
