934b8c17b156e53570fddf964925353fffcb7050
Mustafa Muse <umpp101@icloud.com>
2019-12-02 14:43:39 -0800

    Creating all of the Controllers and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :users
+  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
